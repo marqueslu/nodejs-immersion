58b2743af7d8b732eb3e436a459d736522ba2d09
Lucas Marques <lucas.silva@dextra-sw.com>
2021-01-20 15:46:20 -0300

    Event emitter

---
+++ 03-events/index.js
@@ -0,0 +1,32 @@
+const EventEmitter = require('events')
+class MeuEmissor extends EventEmitter {
+
+}
+
+const meuEmissor = new MeuEmissor()
+const nomeEvento = 'usuario:click'
+// meuEmissor.on(nomeEvento, function (click) {
+//     console.log('Um usuário clicou', click)
+// })
+
+// meuEmissor.emit(nomeEvento, 'Na barra de rolagem')
+// meuEmissor.emit(nomeEvento, 'no ok')
+
+// let count = 0
+// setInterval(function () {
+//     meuEmissor.emit(nomeEvento, 'no ok' + (count++))
+// }, 1000)
+
+const stdin = process.openStdin()
+function main() {
+    return new Promise(function (resolve, reject) {
+        stdin.addListener('data', function (value) {
+            // console.log(`Voce digitou: ${value.toString().trim()}`)
+            return resolve(value)
+        })
+    })
+}
+
+main().then(function (resultado) {
+    console.log('resultado', resultado.toString())
+})
